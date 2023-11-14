5d937c1f55d98240795e56997bc37c6849d6963a
Sophie Haider <90757246+sophiehaider@users.noreply.github.com>
2023-11-14 11:03:24 +0100

    added backend changes

--- leohoot-backend/Hubs/Hub.cs
+++ leohoot-backend/Hubs/Hub.cs
@@ -6,10 +6,28 @@ namespace SignalRWebpack.Hubs;
 
 public class ChatHub : Hub
 {
-    public override async Task OnConnectedAsync()
-    {
-        await SendMessage("Hello from the Hub!");
+    List<User> users = new List<User>();
+
+    public ChatHub() {
+        users.Add(new User("Leo", 100));
+        users.Add(new User("Jason", 200));
+        users.Add(new User("Piper", 50));
+        users.Add(new User("Percy", 500));
+        users.Add(new User("Annabeth", 300));
+        users.Add(new User("Frank", 150));
+        users.Add(new User("Hazel", 250));
+        users.Add(new User("Nico", 400));
+        users.Add(new User("Reyna", 350));
+        users.Add(new User("Will", 450));
+        users.Add(new User("Rachel", 550));
+        users.Add(new User("Grover", 600));
+        users.Add(new User("Clarisse", 650));
+        users.Add(new User("Silena", 700));
+        users.Add(new User("Charles", 750));
+
     }
 
-    public async Task SendMessage(string message) => await Clients.All.SendAsync("messageReceived", message);
+    public async Task SendRanking() => await Clients.Caller.SendAsync("rankingReceived", users.OrderByDescending(user => user.Score).Take(5).ToList());
+
+    public async Task SendQuestionIsFinished() => await Clients.All.SendAsync("questionIsFinished");
 }
