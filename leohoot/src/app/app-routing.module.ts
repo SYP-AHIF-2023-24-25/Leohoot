4e1725be970b9524296d9e8e9f5ecbe09fb81ec8
Sophie Haider <90757246+sophiehaider@users.noreply.github.com>
2024-01-23 14:36:00 +0100

    deleted teacher-demo-view and game-teacher-view

--- leohoot/src/app/app-routing.module.ts
+++ leohoot/src/app/app-routing.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TeacherDemoModeQuizComponent } from './components/teacher-demo-mode-quiz/teacher-demo-mode-quiz.component';
 import {StudentsMobileViewComponent} from "./components/students-mobile-view/students-mobile-view.component";
 import { RankingComponent } from './components/ranking/ranking.component';
 import { QuestionComponent } from  './components/question/question.component';
 import { WaitingroomComponent } from './components/waitingroom/waitingroom.component';
 import {StudentsMobileRankingComponent} from "./components/students-mobile-ranking/students-mobile-ranking.component";
-import { GameTeacherViewComponent } from './components/game-teacher-view/game-teacher-view.component';
 import { StudentsLoadingScreenComponent } from './components/students-loading-screen/students-loading-screen.component'
 import { GameLoginComponent } from './components/game-pin-login/game-pin-login.component';
 import { GameUserLoginComponent } from './components/game-user-login/game-user-login.component';
 import { StudentWaitingPageComponent } from './components/students-waiting-page/students-waiting-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/teacherDemoModeQuiz', pathMatch: 'full' },
-  { path: 'teacherDemoModeQuiz', component: TeacherDemoModeQuizComponent },
+  { path: '', redirectTo: '/question', pathMatch: 'full' },
   {path: 'studentMobileView', component: StudentsMobileViewComponent},
   {path: 'studentMobileRanking', component: StudentsMobileRankingComponent},
   { path: 'ranking', component: RankingComponent },
   { path: 'question', component: QuestionComponent },
   { path: 'waitingroom', component: WaitingroomComponent },
-  { path: 'game', component: GameTeacherViewComponent},
   { path: 'studentLoadingScreen', component: StudentsLoadingScreenComponent},
   { path: 'gameLogin', component: GameLoginComponent},
   { path: 'gameUserLogin', component: GameUserLoginComponent},
--- leohoot/src/app/app.module.ts
+++ leohoot/src/app/app.module.ts
@@ -7,21 +7,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeacherDemoModeQuizComponent } from './components/teacher-demo-mode-quiz/teacher-demo-mode-quiz.component';
 import { WaitingroomComponent } from './components/waitingroom/waitingroom.component';
 import { RankingComponent } from './components/ranking/ranking.component';
 import { QuestionComponent } from './components/question/question.component';
 import { StudentsMobileViewComponent } from './components/students-mobile-view/students-mobile-view.component';
 import { StudentsMobileRankingComponent } from './components/students-mobile-ranking/students-mobile-ranking.component';
-import { GameTeacherViewComponent } from './components/game-teacher-view/game-teacher-view.component';
 import { StudentsLoadingScreenComponent } from './components/students-loading-screen/students-loading-screen.component';
 import { GameLoginComponent } from './components/game-pin-login/game-pin-login.component';
 import { GameUserLoginComponent } from './components/game-user-login/game-user-login.component';
 import { StudentWaitingPageComponent } from './components/students-waiting-page/students-waiting-page.component';
-import { HttpClient } from '@microsoft/signalr';
 
 @NgModule({
-  declarations: [AppComponent, TeacherDemoModeQuizComponent, WaitingroomComponent, RankingComponent, StudentsMobileViewComponent, QuestionComponent, StudentsMobileRankingComponent, GameTeacherViewComponent, StudentsLoadingScreenComponent, GameLoginComponent, GameUserLoginComponent, StudentWaitingPageComponent],
+  declarations: [AppComponent, WaitingroomComponent, RankingComponent, StudentsMobileViewComponent, QuestionComponent, StudentsMobileRankingComponent, StudentsLoadingScreenComponent, GameLoginComponent, GameUserLoginComponent, StudentWaitingPageComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, QRCodeModule, NgxQrcodeStylingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
