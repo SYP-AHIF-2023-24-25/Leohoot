fcb51cf0e17ef0ebfa1d04d4e515b4200113d612
Sophie Haider <haider.sophie@icloud.com>
2024-10-07 14:22:07 +0200

    splitted waiting room in components

---
+++ leohoot/src/app/components/waitingroom/game-infos/game-infos.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'app-game-infos',
+  templateUrl: './game-infos.component.html',
+})
+export class GameInfosComponent {
+  @Input() gamePin: number = 0;
+
+}
