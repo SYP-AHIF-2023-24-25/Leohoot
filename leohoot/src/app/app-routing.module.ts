bcc086518a5fa5da679c72657b07d1cdbb6a2cf4
Diabetisch <marceta.mia2006@gmail.com>
2024-11-04 00:01:44 +0100

    dashboard added to overview

--- leohoot/src/app/app-routing.module.ts
+++ leohoot/src/app/app-routing.module.ts
@@ -20,11 +20,13 @@ import {QuestionPreviewComponent} from './screens/teacher-screens/question-previ
 import { QuizMakerQuestionsComponent } from './components/quiz-maker/quiz-maker-questions/quiz-maker-questions.component';
 import { AuthGuard } from './model/auth-guard';
 import { Role } from './model/leo-token';
+import {DashboardComponent} from "./components/teacher-components/dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/quizOverview', pathMatch: 'full' },
   { path: 'answerView', component: AnswerViewComponent },
   { path: 'interimResult', component: InterimResultRankingComponent },
+  {path:'dashboard', component: DashboardComponent},
   {
     path: 'ranking',
     component: RankingComponent,
