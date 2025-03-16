9a14709e440a97bd0f46a87b4234e8d4945c80d7
Sophie Haider <haider.sophie@icloud.com>
2025-03-16 12:44:31 +0100

    corrected development urls

--- leohoot/src/environments/environment.development.ts
+++ leohoot/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://leohoot.htl-leonding.ac.at',
-    cdnUrl: 'https://leohoot.htl-leonding.ac.at'
+    apiUrl: 'http://localhost:5000',
+    cdnUrl: 'http://localhost:80'
 };
